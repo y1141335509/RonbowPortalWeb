9e9898c53642d9a19a7352b3f31a25ddb8a77de9
y1141335509 <y1141335509@gmail.com>
2023-10-27 13:16:31 -0700

    changed collpased checkboxes spaces

--- ronbow-portal-web/src/components/ProjectList/ProjectProfile/Progress/ProgressCollapsed.js
+++ ronbow-portal-web/src/components/ProjectList/ProjectProfile/Progress/ProgressCollapsed.js
@@ -10,6 +10,9 @@ import Typography from '@mui/material/Typography';
 import Switch from '@mui/material/Switch';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Checkbox from '@mui/material/Checkbox';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
 
 
 
@@ -28,6 +31,7 @@ const theme = createTheme({
     fontSize: 14,
     body1: {
       fontSize: '12px',
+      fontWeight: 150,
     },
     body2: {
       fontSize: '14px',
@@ -39,24 +43,53 @@ const theme = createTheme({
 const steps = [
   {
     label: 'Receive Lead',
-    description: <div style={{ fontSize: '12px', }}>
-      <p ><Checkbox checked={true} /> Review Documents</p>
-      <p ><Checkbox checked={true} /> Learn About Clients</p>
-      <p ><Checkbox checked={true} /> Get Inspired</p>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox checked={true} size="small" />
+            Review Documents
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox checked={true} size="small" />
+            Learn About Clients
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox checked={true} size="small" />
+            Get Inspired
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/01/2023',
   },
   {
     label: '1st Meeting',
     description:
-      <div style={{ fontSize: '12px', }}>
-        <p ><Checkbox /> Review Meeting Notes</p>
-        <p ><Checkbox /> Design</p>
-        <p ><Checkbox /> Generate Proposal</p>
-        <p ><Checkbox /> Validate Requirements</p>
-        <p ><Checkbox /> Generate Documents</p>
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Review Documents
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Design
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Generate Proposal
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Validate Requirements
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Generate Documents
+          </ListItem>
+        </List>
       </div>,
-      deadline: 'by 01/04/2023',
+    deadline: 'by 01/04/2023',
   },
   {
     label: 'Present Design & Proposal',
@@ -67,37 +100,79 @@ const steps = [
   },
   {
     label: 'Finalize the Design',
-    description: <div style={{ fontSize: '12px', }}>
-      <p ><Checkbox /> Check Documents</p>
-      <p ><Checkbox /> Receive Payment</p>
-      <p ><Checkbox /> Verify Design</p>
-      <p ><Checkbox /> Customer Confirmed and Paid</p>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Check Documents
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Receive Payment
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Verify Design
+          </ListItem>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Customer Confirmed and Paid
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/09/2023',
   },
   {
     label: 'Submit Project',
-    description: <div style={{ fontSize: '12px', }}>
-      <p >Project Check</p>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Project Check
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/11/2023',
   },
   {
     label: 'Manufacture',
-    description: <div style={{ fontSize: '12px', }}>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Manufacture
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/13/2023',
   },
   {
     label: 'Delivery & Installation',
-    description: <div style={{ fontSize: '12px', }}>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Devliery and Installation
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/14/2023',
   },
   {
     label: 'Celebration',
-    description: <div style={{ fontSize: '12px', }}>
-    </div>,
+    description:
+      <div style={{ fontSize: '12px' }}>
+        <List dense style={{ padding: 0 }}>
+          <ListItem style={{ padding: '0 0 0 0' }}>
+            <Checkbox size="small" />
+            Celebration
+          </ListItem>
+        </List>
+      </div>,
     deadline: 'by 01/21/2023',
   },
 ];
@@ -131,7 +206,7 @@ export default function VerticalLinearStepper() {
             inputProps={{ 'aria-label': 'Expand all steps switch' }}
           />
         </Box>
-        
+
         <Stepper activeStep={activeStep} orientation="vertical">
           {steps.map((step, index) => (
             <Step key={step.label} expanded={allStepsExpanded || (activeStep === index && !allStepsExpanded)}>  {/* Adjust expanded property */}
